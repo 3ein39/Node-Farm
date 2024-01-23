8217194c6de1b2ec961ae890baea4c3ef5834330
Hussein Hany <husseinhany@192.168.1.20>
2024-01-23 09:37:38 +0200

    Finish overview page

--- index.js
+++ index.js
@@ -2,10 +2,30 @@ var http = require('http');
 var url = require('url');
 var fs = require('node:fs');
 var productsData = fs.readFileSync("".concat(__dirname, "/dev-data/data.json"), 'utf-8');
+var productsDataObj = JSON.parse(productsData);
+// read the template files
+// they are read only once when starting the server
+var product_template = fs.readFileSync("".concat(__dirname, "/templates/product.html"), 'utf-8');
+var overview_template = fs.readFileSync("".concat(__dirname, "/templates/overview.html"), 'utf-8');
+var card_template = fs.readFileSync("".concat(__dirname, "/templates/card.html"), 'utf-8');
+var fillCard = function (data, template) {
+    var output = template.replace(/{%IMAGE%}/g, data.image);
+    output = output.replace(/{%PRODUCT_NAME%}/g, data.productName);
+    output = output.replace(/{%PRICE%}/g, data.price);
+    output = output.replace(/{%QUANTITY%}/g, data.quantity);
+    output = output.replace(/{%ID%}/g, data.id);
+    if (data.organic === false)
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    // console.log(output);
+    return output;
+};
 var server = http.createServer(function (req, res) {
     var pathName = req.url;
     if (pathName === '/' || pathName === '/overview') {
-        res.end("Welcome to OVERVIEW page");
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        var cardsHTML = productsDataObj.map(function (el) { return fillCard(el, card_template); }).join('');
+        var output = overview_template.replace(/{%PRODUCT_CARDS%}/g, cardsHTML);
+        res.end(output);
     }
     else if (pathName === '/products') {
         res.end("Welcome to PRODUCTS page");
--- index.ts
+++ index.ts
@@ -4,13 +4,35 @@ const fs = require('node:fs');
 
 
 const productsData = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const productsDataObj = JSON.parse(productsData);
+// read the template files
+// they are read only once when starting the server
+const product_template = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
+const overview_template = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
+const card_template = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
 
+const fillCard = (data, template) => {
+    let output = template.replace(/{%IMAGE%}/g, data.image);
+    output = output.replace(/{%PRODUCT_NAME%}/g, data.productName);
+    output = output.replace(/{%PRICE%}/g, data.price);
+    output = output.replace(/{%QUANTITY%}/g, data.quantity);
+    output = output.replace(/{%ID%}/g, data.id);
+
+    if (data.organic === false)
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+    // console.log(output);
+    return output;
+}
 
 const server =  http.createServer((req, res) => {
     const pathName = req.url;
 
     if (pathName === '/' || pathName === '/overview') {
-        res.end(`Welcome to OVERVIEW page`);
+        res.writeHead(200, {'Content-type': 'text/html'});
+
+        const cardsHTML = productsDataObj.map(el => fillCard(el, card_template)).join('');
+        const output = overview_template.replace(/{%PRODUCT_CARDS%}/g, cardsHTML);
+        res.end(output);
     }
     else if (pathName === '/products') {
         res.end(`Welcome to PRODUCTS page`)
