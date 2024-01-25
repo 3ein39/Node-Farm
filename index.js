5fdb25e08d2feebd5dd62628c7f9381e59c3fb7a
Hussein Hany <hussein.hany.cs@gmail.com>
2024-01-25 13:54:46 +0200

    Reformat code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Import necessary modules
-var http = require('http');
-var url = require('url');
-var fs = require('node:fs');
+var http = require("http");
+var url = require("url");
+var fs = require("node:fs");
 // Read and parse the product data from the JSON file
-var productsData = fs.readFileSync("".concat(__dirname, "/dev-data/data.json"), 'utf-8');
+var productsData = fs.readFileSync("".concat(__dirname, "/dev-data/data.json"), "utf-8");
 var productsDataObj = JSON.parse(productsData);
 // Read the HTML templates for the product, overview, and card
-var product_template = fs.readFileSync("".concat(__dirname, "/templates/product.html"), 'utf-8');
-var overview_template = fs.readFileSync("".concat(__dirname, "/templates/overview.html"), 'utf-8');
-var card_template = fs.readFileSync("".concat(__dirname, "/templates/card.html"), 'utf-8');
+var product_template = fs.readFileSync("".concat(__dirname, "/templates/product.html"), "utf-8");
+var overview_template = fs.readFileSync("".concat(__dirname, "/templates/overview.html"), "utf-8");
+var card_template = fs.readFileSync("".concat(__dirname, "/templates/card.html"), "utf-8");
 // Function to replace placeholders in the HTML templates with actual product data
 var fillCard = function (data, template) {
     // Replace placeholders with actual data
@@ -22,7 +22,7 @@ var fillCard = function (data, template) {
     output = output.replace(/{%DESCRIPTION%}/g, data.description);
     // If the product is not organic, add 'not-organic' class
     if (data.organic === false)
-        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+        output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
     return output;
 };
 // Create the server
@@ -30,41 +30,41 @@ var server = http.createServer(function (req, res) {
     // Parse the URL to get the path and query parameters
     var completeUrl = "http://".concat(req.headers.host).concat(req.url);
     var url = new URL(completeUrl);
-    var pathName = url.pathname || '/';
-    var productID = url.searchParams.get('id');
-    var oh = 'wow';
-    var oh2 = 'wow';
+    var pathName = url.pathname || "/";
+    var productID = url.searchParams.get("id");
+    var oh = "wow";
+    var oh2 = "wow";
     // Handle different routes
-    if (pathName === '/' || pathName === '/overview') {
+    if (pathName === "/" || pathName === "/overview") {
         // Send the overview page
-        res.writeHead(200, { 'Content-type': 'text/html' });
+        res.writeHead(200, { "Content-type": "text/html" });
         // Generate HTML for each product card and insert it into the overview template
-        var cardsHTML = productsDataObj.map(function (el) { return fillCard(el, card_template); }).join('');
+        var cardsHTML = productsDataObj.map(function (el) { return fillCard(el, card_template); }).join("");
         var output = overview_template.replace(/{%PRODUCT_CARDS%}/g, cardsHTML);
         res.end(output);
     }
     else if (pathName === "/product") {
         // Send the product page
-        res.writeHead(200, { 'Content-type': 'text/html' });
+        res.writeHead(200, { "Content-type": "text/html" });
         // Get the requested product and generate the product page HTML
         if (productID === null)
-            productID = '0';
+            productID = "0";
         var product = productsDataObj[productID];
         var output = fillCard(product, product_template);
         res.end(output);
     }
-    else if (pathName === '/api') {
+    else if (pathName === "/api") {
         // Send the raw product data as JSON
-        res.writeHead(200, { 'Content-type': 'application/json' });
+        res.writeHead(200, { "Content-type": "application/json" });
         res.end(productsData);
     }
     else {
         // Send a 404 error for any other path
         res.writeHead(404);
-        res.end('wrong path');
+        res.end("wrong path");
     }
 });
 // Start the server
-server.listen(8000, '127.0.0.1', function () {
-    console.log('Listening to requests on port 8000');
+server.listen(8000, "127.0.0.1", function () {
+    console.log("Listening to requests on port 8000");
 });
--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
 // Import necessary modules
-const http = require('http');
-const url = require('url');
-const fs = require('node:fs');
+const http = require("http");
+const url = require("url");
+const fs = require("node:fs");
 
 // Read and parse the product data from the JSON file
-const productsData = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const productsData = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const productsDataObj = JSON.parse(productsData);
 
 // Read the HTML templates for the product, overview, and card
-const product_template = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
-const overview_template = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
-const card_template = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
+const product_template = fs.readFileSync(`${__dirname}/templates/product.html`, "utf-8");
+const overview_template = fs.readFileSync(`${__dirname}/templates/overview.html`, "utf-8");
+const card_template = fs.readFileSync(`${__dirname}/templates/card.html`, "utf-8");
 
 // Function to replace placeholders in the HTML templates with actual product data
 const fillCard = (data: any, template: any) => {
@@ -25,7 +25,7 @@ const fillCard = (data: any, template: any) => {
   output = output.replace(/{%DESCRIPTION%}/g, data.description);
 
   // If the product is not organic, add 'not-organic' class
-  if (data.organic === false) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  if (data.organic === false) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
 
   return output;
 };
@@ -36,42 +36,42 @@ const server = http.createServer((req: any, res: any) => {
   // Parse the URL to get the path and query parameters
   const completeUrl = `http://${req.headers.host}${req.url}`;
   const url = new URL(completeUrl);
-  const pathName = url.pathname || '/';
-  let productID = url.searchParams.get('id');
+  const pathName = url.pathname || "/";
+  let productID = url.searchParams.get("id");
 
-  let oh: string = 'wow';
-  let oh2: string = 'wow';
+  let oh: string = "wow";
+  let oh2: string = "wow";
   // Handle different routes
-  if (pathName === '/' || pathName === '/overview') {
+  if (pathName === "/" || pathName === "/overview") {
     // Send the overview page
-    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.writeHead(200, { "Content-type": "text/html" });
 
     // Generate HTML for each product card and insert it into the overview template
-    const cardsHTML = productsDataObj.map((el: any) => fillCard(el, card_template)).join('');
+    const cardsHTML = productsDataObj.map((el: any) => fillCard(el, card_template)).join("");
     const output = overview_template.replace(/{%PRODUCT_CARDS%}/g, cardsHTML);
     res.end(output);
   } else if (pathName === `/product`) {
     // Send the product page
-    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.writeHead(200, { "Content-type": "text/html" });
 
     // Get the requested product and generate the product page HTML
-    if (productID === null) productID = '0';
+    if (productID === null) productID = "0";
     const product = productsDataObj[productID];
     const output = fillCard(product, product_template);
     res.end(output);
-  } else if (pathName === '/api') {
+  } else if (pathName === "/api") {
     // Send the raw product data as JSON
-    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.writeHead(200, { "Content-type": "application/json" });
     res.end(productsData);
   } else {
     // Send a 404 error for any other path
     res.writeHead(404);
-    res.end('wrong path');
+    res.end("wrong path");
   }
 
 });
 
 // Start the server
-server.listen(8000, '127.0.0.1', () => {
-  console.log('Listening to requests on port 8000');
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
 });
